316952b877c1c2b851cefed30210fee68b280b81
Michael Krasnow <mnkras@gmail.com>
2014-10-26 02:10:52 -0400

    dat 5.7 stuff

--- controller.php
+++ controller.php
@@ -1,17 +1,25 @@
-<?php    defined('C5_EXECUTE') or die(_("Access Denied."));
+<?php
 
+namespace Concrete\Package\PageRedirect;
+
+defined('C5_EXECUTE') or die("Access Denied.");
+
+use Loader;
+use \Concrete\Core\Attribute\Type as AttributeType;
+use \Concrete\Core\Attribute\Key\CollectionKey as CollectionAttributeKey;
+use Events;
 /**
  * Class that is used to redirect based on a page attribute
  * @package Page Redirect
  * @author Michael Krasnow <mnkras@gmail.com>
  * @category Packages
- * @copyright  Copyright (c) 2012 Michael Krasnow. (http://www.mnkras.com)
+ * @copyright  Copyright (c) 2014 Michael Krasnow. (http://www.mnkras.com)
  */
-class PageRedirectPackage extends Package {
+class Controller extends \Concrete\Core\Package\Package {
 
 	protected $pkgHandle = 'page_redirect';
-	protected $appVersionRequired = '5.4.1';
-	protected $pkgVersion = '1.4';
+	protected $appVersionRequired = '5.7.1';
+	protected $pkgVersion = '2.0';
 	
 	public function getPackageDescription() {
 		return t("Adds a page attribute that allows you to specify a page to redirect to.");
@@ -22,10 +30,9 @@ class PageRedirectPackage extends Package {
 	}
 	
 	public function install() {
-		Loader::model('collection_attributes');		 
 		$PageAttrType = AttributeType::getByHandle('page_selector');
 		if(!is_object($PageAttrType) || !intval($PageAttrType->getAttributeTypeID())){ 
-			throw new exception(t('Please install %s before installing this addon.', '<a href="http://www.concrete5.org/marketplace/addons/page-selector-attribute/">Page Selector Attribute</a>'));
+			throw new \Exception(t('Please install %s before installing this addon.', '<a href="http://www.concrete5.org/marketplace/addons/page-selector-attribute/">Page Selector Attribute</a>'));
 			exit;
 		}
 		$pkg = parent::install();
@@ -33,7 +40,30 @@ class PageRedirectPackage extends Package {
 	}
 	
 	public function on_start() {
-		$url = Loader::helper('concrete/urls')->getPackageURL(Package::getByHandle('page_redirect'));
-		Events::extend('on_start', 'PageRedirect', 'checkredirect', DIRNAME_PACKAGES . '/' . $this->pkgHandle . '/models/page_redirect.php');
+		Events::addListener('on_start', function($event) {
+            $page = \Page::getCurrentPage();
+            if(!is_object($page)) {
+                return;
+            }
+            //get attribute
+            $page_selector = $page->getAttribute('page_selector_redirect');
+            //start checking if its a valid page
+            if($page_selector > 0) {
+                $npage = \Page::getByID($page_selector);
+                //more checking
+                if(is_object($npage) && !$npage->isError()) {
+                    //redirect
+                    header("HTTP/1.1 301 Moved Permanently");
+                    if(!$npage->isExternalLink()) {
+                        $nh = Loader::Helper('navigation');
+                        header('Location: '.$nh->getLinkToCollection($npage, true));
+                        exit;
+                    } else {
+                        header('Location: '.$npage->getCollectionPointerExternalLink());
+                        exit;
+                    }
+                }
+            }
+        });
 	}
 }
